16f8c09944d568fb504ec35429e920434cd7fbd9
Prateek <r.prateek11@gmail.com>
2021-06-02 21:41:12 +0530

    Initial Commit

---
+++ dist/__tests__/acceptance/test-helper.d.ts
@@ -0,0 +1,7 @@
+import { BoilerPlateApplication } from '../..';
+import { Client } from '@loopback/testlab';
+export declare function setupApplication(): Promise<AppWithClient>;
+export interface AppWithClient {
+    app: BoilerPlateApplication;
+    client: Client;
+}
---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+import { ApplicationConfig, BoilerPlateApplication } from './application';
+export * from './application';
+export declare function main(options?: ApplicationConfig): Promise<BoilerPlateApplication>;
